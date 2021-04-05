49e832e284aea1991e0e67f120c5b0ac8847e0ce
Jean Paulo Silva Vasconcelos <jean.vasconcelos6@gmail.com>
2021-04-05 11:02:27 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+const SUCCESS = 200;
+const PORT = '3000';
+
+// não remova esse endpoint, e para o avaliador funcionar
+app.get('/', (_request, response) => {
+  response.status(SUCCESS).send();
+});
+
+app.listen(PORT, () => { console.log('Online'); });
