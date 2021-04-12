fe9b5fedb9d07f490ee8cd9be33fff52b9c58d67
Israel Menon <zezomenon@gmail.com>
2021-04-12 07:20:40 -0300

    Desafio 3, incompleto, falta corrigir erro de validacao de password

--- src/routes/login.js
+++ src/routes/login.js
@@ -5,28 +5,35 @@ const router = express.Router();
 
 const randomToken = () => crypto.randomBytes(8).toString('hex');
 
-const emailValidate = (email) => {
+const emailValid = (email) => {
   const regex = /^[^@]+@\w+(\.\w+)+\w$/;
-  return regex.test(String(email).toLowerCase);
+  return regex.test(String(email).toLowerCase());
 };
 
-const passwordValidate = (password) => {
-  const regex = /^[0-9a-zA-Z]{6,}$/;
-  return regex.test(password);
+const passwordValid = (password) => {
+    const regex = /^[0-9a-zA-Z]{6,}$/;
+    return regex.test(password);
 };
 
-router.post('/', (req, res) => {
+const validateEmailAndPassowrd = (req, res, next) => {
+  const emailDontExist = 'O campo "email" é obrigatório';
+  const emailInvalidFormat = 'O "email" deve ter o formato "email@email.com"';
+  const passwordDontExist = 'O campo "password" é obrigatório';
+  const passwordInvalidSize = 'A "senha" deve ter pelo menos 6 caracteres';
+
   const { email, password } = req.body;
-  try {
-    const token = randomToken();
-    emailValidate(email);
-    passwordValidate(password);
-    // tratar erro 404, nao fiz ainda
+  console.log(passwordValid);
+  if (!email) return res.status(400).json({ message: emailDontExist });
+  if (!emailValid(email)) return res.status(400).json({ message: emailInvalidFormat });
+  if (!password) return res.status(400).json({ message: passwordDontExist }); // nao ta passando
+  if (!passwordValid(password)) return res.status(400).json({ message: passwordInvalidSize });
 
-    res.status(201).json({ token });
-  } catch (error) {
-    throw new Error(error);
-  }
+  next();
+};
+
+router.post('/', validateEmailAndPassowrd, (_req, res) => {
+  const token = randomToken();
+  res.status(201).json({ token });
 });
 
 module.exports = router;
